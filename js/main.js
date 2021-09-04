1ad97d6dc3304861825414b29b8f5cc5e784f3ef
AFeethamDev <feetham.al@gmail.com>
2021-09-03 22:56:59 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.querySelector('button').addEventListener('click',getHS);
+
+function isChecked(str){
+  return document.getElementById(str).checked == true;
+}
+
+function getHS(){
+  let chosenCard = document.querySelector('input').value;
+  let searchParameters;
+  let selectMode;
+
+  isChecked('cardName') ? searchParameters = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/" : searchParameters = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/races/"
+  isChecked('constructed') ? selectMode = "?collectible=1" : selectMode = "";
+  document.querySelector('#myImgs').innerHTML = '';
+
+  fetch(searchParameters+chosenCard+selectMode, {
+	   "method": "GET",
+	    "headers": {
+		      "x-rapidapi-key": "ceaa1d9eedmshdc4bcc96e0d8f2bp14ea31jsn0b7a3eacafb7",
+		        "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+	         }
+         })
+         .then(res => res.json())
+         .then(data => {
+           if(isChecked('battlegrounds')){
+             data = data.filter(x => x.cardSet == "Battlegrounds");
+           }
+	          console.log(data);
+            for(let i = 0; i < data.length; i++){
+               if(data[i].img){
+                document.querySelector('#myImgs').innerHTML += ('<img src='+data[i].img+' alt="'+data[i].cardSet+'" title="'+data[i].cardSet+'">');
+              }
+            }
+          })
+          .catch(err => {
+	           console.error(err);
+           });
+}
